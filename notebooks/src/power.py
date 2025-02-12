1f394ec2d92c816f270705571e853da41bdec825
Will Decker <will.decker@gatech.edu>
2025-02-12 16:46:58 -0500

    rm extra whitespace

--- notebooks/src/power.py
+++ notebooks/src/power.py
@@ -2,7 +2,6 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
 
-
 def grid(**kwargs):
     """Generate all possible combinations of elements in K arrays"""
     return np.array(np.meshgrid(*list(kwargs.values()))).T.reshape(-1, len(kwargs))
