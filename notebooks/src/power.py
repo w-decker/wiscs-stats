28c3a3fa48d0e0e09b82adf5163ffa5bb4a48c63
Will Decker <will.decker@gatech.edu>
2025-03-09 15:05:03 -0400

    make docstring better

--- notebooks/src/power.py
+++ notebooks/src/power.py
@@ -3,5 +3,11 @@ import warnings
 warnings.filterwarnings("ignore")
 
 def grid(**kwargs):
-    """Generate all possible combinations of elements in K arrays"""
+    """Generate all possible combinations of elements in K arrays
+    
+    **kwargs
+    -------
+    list, np.ndarray
+        K arrays of elements to combine
+    """
     return np.array(np.meshgrid(*list(kwargs.values()))).T.reshape(-1, len(kwargs))
